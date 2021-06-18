7c35233a60004b8f5eacd5a3d290b8bed6fa9318
ethedev <2432272+ethedev@users.noreply.github.com>
2021-06-18 08:18:29 +0400

    Update tests/sdk.test.ts

--- tests/sdk.test.ts
+++ tests/sdk.test.ts
@@ -84,17 +84,6 @@ describe('General function testing', () => {
     expect(typeof apr).toBe("number");
   });
 
-  /**
-   * @notice The following method tests should be abandoned in the future.
-   * To see the test results, please uncomment them.
-   */
-  // it("getAPR should return a number", async () => {
-  //   /// @TODO Pass right params
-  //   const apr = await SDK.apr.getMiningRewards('uGAS-JUN21', Assets[network]["ugas"][3], 107.5);
-  //   // console.debug("apr", apr)
-  //   expect(typeof apr).toBe("string");
-  // });
-
   // it("getUserStats must return an object", async () => {
   //   const userStats = await SDK.stats.getUserStats(1623619086, 1623885486);
   //   // console.debug("stats", userStats)
