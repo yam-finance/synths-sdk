ee5517fd684bc90353b8a178d444318c76499968
byterose <>
2021-06-23 15:19:19 +0200

    Add asset to apr calc

--- src/utils/apr.ts
+++ src/utils/apr.ts
@@ -298,6 +298,8 @@ export class MiningRewards {
         assets["ugas"][3].emp.address,
         /* @ts-ignore */
         assets["ustonks"][0].emp.address,
+        /* @ts-ignore */
+        assets["ustonks"][1].emp.address,
       ];
       const umadata: any = await fetch(
         `https://raw.githubusercontent.com/UMAprotocol/protocol/master/packages/affiliates/payouts/devmining-status.json`
