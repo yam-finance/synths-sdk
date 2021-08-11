b4f37ed7c3fdac4342b357fee629756448717208
Ramy Melo <ramysmelo@gmail.com>
2021-08-10 21:56:27 -0400

    cleaned up SynthsSdk tests.
    Modified getGCR test. Now asserts that the GCR be higher than the minimum CR(1.05)

--- test/SynthsSdk.test.ts
+++ test/SynthsSdk.test.ts
@@ -1,97 +1,80 @@
-import { ethers, artifacts, waffle } from "hardhat";
-import { Signer, Contract, BigNumber } from "ethers";
-import { MockProvider } from "@ethereum-waffle/provider";
-import { expect } from "chai";
-import Synths from "../src";
-import { INFURA_API_KEY } from "../src/lib/config";
-import { SynthsAssetsConfig, EmpState } from "../src/types/assets.t";
+import {ethers} from "hardhat";
+import {BigNumber} from "ethers";
+import {expect} from "chai";
+import Synths from "~/index";
+import {SynthsAssetsConfig} from "~/types/assets.t";
+import Asset from "~/lib/Asset";
 
-async function deploy(name: string, ...params: any) {
-  const Contract = await ethers.getContractFactory(name);
-  return await Contract.deploy(...params).then((f) => f.deployed());
-}
+describe("Synths SDKs", function () {
+    let provider: typeof ethers.provider;
 
-describe("Synths SDK", function () {
-  // let provider: MockProvider;
-  let provider: any;
-  let accounts: Signer[];
+    describe("Ethereum Mainnet tests", function () {
+        let upunksAsset: Asset;
 
-  // beforeEach(async function () {
-  //   provider = waffle.provider;
-  //   accounts = await ethers.getSigners();
-  // });
+        before(async function () {
+            provider = ethers.provider
+            const chainId = (await provider.getNetwork()).chainId;
+            const userAssetsConfig: SynthsAssetsConfig = {
+                [chainId]: {
+                    upunks: [
+                        {
+                            name: "September",
+                            cycle: "09",
+                            year: "21",
+                            collateral: "WETH",
+                            token: {
+                                address: "0x86140A763077155964754968B6F6e243fE809cBe",
+                                decimals: 18,
+                            },
+                            emp: {
+                                address: "0xF8eF02C10C473CA5E48b10c62ba4d46115dd2288",
+                                new: true,
+                                type: "finlib",
+                            },
+                            pool: {
+                                address: "0x6e01db46b183593374a49c0025e42c4bb7ee3ffa",
+                                location: "sushiswap",
+                            },
+                            expired: false,
+                        },
+                    ],
+                },
+            };
 
-  describe("Ethereum Mainnet tests", function () {
-    let upunksAsset: any;
+            const synthsSDK = await Synths.create({
+                ethersProvider: provider,
+                userAssetsConfig: userAssetsConfig,
+            });
 
-    before(async function () {
-      // provider = waffle.provider;
-      provider = new ethers.providers.JsonRpcProvider(
-        `https://mainnet.infura.io/v3/${INFURA_API_KEY}`
-      );
-      accounts = await ethers.getSigners();
-      const chainId: number = (await provider.getNetwork()).chainId;
-      const userAssetsConfig: SynthsAssetsConfig = {
-        [chainId]: {
-          upunks: [
-            {
-              name: "September",
-              cycle: "09",
-              year: "21",
-              collateral: "WETH",
-              token: {
-                address: "0x86140A763077155964754968B6F6e243fE809cBe",
-                decimals: 18,
-              },
-              emp: {
-                address: "0xF8eF02C10C473CA5E48b10c62ba4d46115dd2288",
-                new: true,
-                type: "finlib",
-              },
-              pool: {
-                address: "0x6e01db46b183593374a49c0025e42c4bb7ee3ffa",
-                location: "sushiswap",
-              },
-              expired: false,
-            },
-          ],
-        },
-      };
-
-      const synthsSDK = await Synths.create({
-        ethersProvider: provider,
-        userAssetsConfig: userAssetsConfig,
-      });
-
-      upunksAsset = await synthsSDK.connectAsset("upunks-0921");
-    });
-
-    // @todo Add tests
-    describe("Interact with asset", function () {
-      it("getEmpState - success", async function () {
-        const empState: EmpState = await upunksAsset.getEmpState();
-        expect(empState).to.deep.include({
-          collateralCurrency: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+            upunksAsset = await synthsSDK.connectAsset("upunks-0921");
         });
-      });
-      it("getPosition - success", async function () {
-        const position: any = await upunksAsset.getPosition();
-        expect(position.rawCollateral["rawValue"]).to.equal(BigNumber.from(0));
-      });
-      it("getPositionCR - success", async function () {
-        const positionCR: any = await upunksAsset.getPositionCR();
-        expect(positionCR).to.equal("0");
-      });
-      it("getPositions - success", async function () {
-        const positions: any = await upunksAsset.getPositions();
-        expect(positions).to.deep.include({
-          "0x86140A763077155964754968B6F6e243fE809cBe": BigNumber.from(0),
+
+        // @todo Add tests
+        describe("Interact with asset", function () {
+            it("getEmpState - success", async function () {
+                const empState = await upunksAsset.getEmpState();
+                expect(empState).to.deep.include({
+                    collateralCurrency: "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+                });
+            });
+            it("getPosition - success", async function () {
+                const position = await upunksAsset.getPosition();
+                expect(position?.rawCollateral["rawValue"]).to.equal(BigNumber.from(0));
+            });
+            it("getPositionCR - success", async function () {
+                const positionCR = await upunksAsset.getPositionCR();
+                expect(positionCR).to.equal("0");
+            });
+            it("getPositions - success", async function () {
+                const positions = await upunksAsset.getPositions();
+                expect(positions).to.deep.include({
+                    "0x86140A763077155964754968B6F6e243fE809cBe": BigNumber.from(0),
+                });
+            });
+            it("getGCR - success", async function () {
+                const gcr = await upunksAsset.getGCR();
+                expect(parseFloat(gcr ?? "0")).to.be.greaterThan(1.05);
+            });
         });
-      });
-      it("getGCR - success", async function () {
-        const gcr: any = await upunksAsset.getGCR();
-        // console.log(gcr);
-      });
     });
-  });
 });
