b48149bb53de7fd1e7f83e8565a386d211c4a680
byterose <>
2021-08-27 14:34:26 +0200

    Update import

--- src/lib/config/index.ts
+++ src/lib/config/index.ts
@@ -1,4 +1,4 @@
-import { SynthsAssetsConfig } from "~/types/assets.t";
+import { SynthsAssetsConfig } from "../../types/assets.t";
 import assets from "../../assets.json";
 import dotenv from "dotenv";
 
