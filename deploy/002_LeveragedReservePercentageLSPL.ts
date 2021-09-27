fdf6e2b366fe747a0564cd47e4759d2415c322ce
Ramy Melo <ramysmelo@gmail.com>
2021-09-27 10:40:21 -0400

    Added LeveragedReservePercentageLSPL contract, deploy script and coverage tests.

---
+++ deploy/002_LeveragedReservePercentageLSPL.ts
@@ -0,0 +1,15 @@
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { getNamedAccounts, deployments } = hre;
+  const { deployer } = await getNamedAccounts();
+
+  await deployments.deploy("LeveragedReservePercentageLSPL", {
+    from: deployer,
+    args: [],
+  });
+};
+export default func;
+
+func.tags = ["LeveragedReservePercentageLSPL"];
