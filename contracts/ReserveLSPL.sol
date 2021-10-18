d4b6f8d2d40333813566f798419a9e7c26df0551
Ramy Melo <ramysmelo@gmail.com>
2021-10-18 12:05:58 -0400

    Updated pragmas.

--- contracts/ReserveLSPL.sol
+++ contracts/ReserveLSPL.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: AGPL-3.0-only
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 import "@uma/core/contracts/financial-templates/common/financial-product-libraries/long-short-pair-libraries/LongShortPairFinancialProductLibrary.sol";
 import "@uma/core/contracts/common/implementation/Lockable.sol";
