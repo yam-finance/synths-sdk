46e6273f8e7e90a4b94ea6e281ffa7c8cfaca6a6
byterose <byterose@tutanota.com>
2021-11-15 21:35:58 +0100

    Update test

--- test/SynthsSdk.test.ts
+++ test/SynthsSdk.test.ts
@@ -100,11 +100,11 @@ describe("Synths SDKs", function () {
       lspAsset = synthsSDK.connectAsset("2xdpi-1021");
     });
     describe("Interact with LSP asset", () => {
-      it("getLSPPortfolio - Success", async function () {
-        this.timeout(100000);
+      // it("getLSPPortfolio - Success", async function () {
+        // this.timeout(100000);
         // const portfolio = await synthsSDK.getLSPPortfolio();
         // expect(portfolio).to.be.an("object");
-      });
+      // });
       it("getLSPState - Success", async function () {
         const lspState = await lspAsset.getLSPState();
         expect(lspState).to.deep.include({
