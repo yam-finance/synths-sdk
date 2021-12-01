b82d098644226cc9778c7ee8b2f83c8f580181b5
byterose <byterose@tutanota.com>
2021-12-02 00:17:01 +0100

    Update config

--- src/lib/config/index.ts
+++ src/lib/config/index.ts
@@ -1,10 +1,7 @@
 import type { SynthsAssetsConfig } from "../../types/assets.t";
 import assets from "../../assets.json";
 import assetstest from "../../assetstest.json";
-import dotenv from "dotenv";
 
-dotenv.config();
-
-export const POLYSCAN_API_KEY = process.env["POLYSCAN_API_KEY"] || "";
+export const POLYSCAN_API_KEY = process.env.VUE_APP_POLYSCAN_API_KEY
 export const defaultAssetsConfig: SynthsAssetsConfig = assets;
 export const defaultTestAssetsConfig: SynthsAssetsConfig = assetstest;
