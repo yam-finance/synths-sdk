12818f1a9942eb8e7c2d39c4b2fa5160d1468eae
byterose <byterose@tutanota.com>
2021-12-02 12:19:33 +0100

    Hardcode api key

--- src/lib/config/index.ts
+++ src/lib/config/index.ts
@@ -2,6 +2,7 @@ import type { SynthsAssetsConfig } from "../../types/assets.t";
 import assets from "../../assets.json";
 import assetstest from "../../assetstest.json";
 
-export const POLYSCAN_API_KEY = process.env.VUE_APP_POLYSCAN_API_KEY
+// export const POLYSCAN_API_KEY = process.env.VUE_APP_POLYSCAN_API_KEY
+export const POLYSCAN_API_KEY = "SSMPT5YB4HKG5NM8Y7SP7Q433GIV94H96U";
 export const defaultAssetsConfig: SynthsAssetsConfig = assets;
 export const defaultTestAssetsConfig: SynthsAssetsConfig = assetstest;
